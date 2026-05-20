b4635cad49615cc11856c132b2c9fbbef8816436
RJdot12 <Radislavyagnyuk11@gmail.com>
2026-05-20 19:52:41 +0300

    Update socials.ts

--- src/config/socials.ts
+++ src/config/socials.ts
@@ -2,7 +2,7 @@ export const socialLinks = {
   telegram: "https://t.me/juristnaplavu",
   instagram: "https://www.instagram.com/juristnaplavu12/",
   viber: "https://connect.viber.com/business/1107b2cc-eccf-11f0-ad17-1222c917034a",
-  whatsapp: "https://wa.me/qr/J6534YSOWGJRC1",
+  whatsapp: "https://wa.me/qr/NPISDM3JNKZUM1",
   facebook: "https://www.facebook.com/profile.php?id=61585780606036",
   tiktok: "https://www.tiktok.com/@juristnaplavu",
   youtube: "https://www.youtube.com/@juristnaplavu",
